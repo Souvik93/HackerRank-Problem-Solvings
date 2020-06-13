185c51295a699bd7b56c1835ac805c3b12fc9d80
Souvik <souvik.das@live.in>
2020-06-13 17:52:56 +0530

    File Creation
    Question & Solution added....

---
+++ Cut_the_sticks.java
@@ -0,0 +1,152 @@
+/*
+
+You are given a number of sticks of varying lengths. You will iteratively cut the sticks into smaller sticks, discarding the shortest pieces until there are none left. At each iteration you will determine the length of the shortest stick remaining, cut that length from each of the longer sticks and then discard all the pieces of that shortest length. When all the remaining sticks are the same length, they cannot be shortened so discard them.
+
+Given the lengths of  sticks, print the number of sticks that are left before each iteration until there are none left.
+
+For example, there are  sticks of lengths . The shortest stick length is , so we cut that length from the longer two and discard the pieces of length . Now our lengths are . Again, the shortest stick is of length , so we cut that amount from the longer stick and discard those pieces. There is only one stick left, , so we discard that stick. Our lengths are .
+
+Function Description
+
+Complete the cutTheSticks function in the editor below. It should return an array of integers representing the number of sticks before each cut operation is performed.
+
+cutTheSticks has the following parameter(s):
+
+arr: an array of integers representing the length of each stick
+Input Format
+
+The first line contains a single integer , the size of .
+The next line contains  space-separated integers, each an  where each value represents the length of the  stick.
+
+Output Format
+
+For each operation, print the number of sticks that are present before the operation on separate lines.
+
+Constraints
+
+Sample Input 0
+
+6
+5 4 4 2 2 8
+Sample Output 0
+
+6
+4
+2
+1
+Explanation 0
+
+sticks-length        length-of-cut   sticks-cut
+5 4 4 2 2 8             2               6
+3 2 2 _ _ 6             2               4
+1 _ _ _ _ 4             1               2
+_ _ _ _ _ 3             3               1
+_ _ _ _ _ _           DONE            DONE
+Sample Input 1
+
+8
+1 2 3 4 3 3 2 1
+Sample Output 1
+
+8
+6
+4
+1
+Explanation 1
+
+sticks-length         length-of-cut   sticks-cut
+1 2 3 4 3 3 2 1         1               8
+_ 1 2 3 2 2 1 _         1               6
+_ _ 1 2 1 1 _ _         1               4
+_ _ _ 1 _ _ _ _         1               1
+_ _ _ _ _ _ _ _       DONE            DONE
+
+
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the cutTheSticks function below.
+    static int[] cutTheSticks(int[] arr) {
+
+Arrays.sort(arr);
+        List<Integer> list = new ArrayList<>();
+       int arrLength = arr.length;
+       list.add(arrLength);
+       int i=0;
+       while(i<arrLength) {
+
+           int num = arr[i];
+           arr[i]=num-arr[i];
+           int counter = 0;
+           for(int j=i+1;j<arrLength;j++) {
+               if(num==arr[j]) {
+                   arr[j]=0;
+                   i=j;
+               } else {
+                   arr[j]=arr[j]-num;
+                   counter++;
+               }
+           }
+           if(counter!=0) {
+               list.add(counter);
+           }
+           i++;
+           
+       }
+
+       int newArr[] = new int[list.size()];
+
+       for(int k=0; k<list.size();k++) {
+           newArr[k]=list.get(k);
+       }
+
+       return newArr;
+
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int[] result = cutTheSticks(arr);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
